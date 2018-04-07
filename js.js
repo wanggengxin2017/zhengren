c016d3c665940a06801fc5f43ccab782717a07b3
wanggengxin2017 <33368080+wanggengxin2017@users.noreply.github.com>
2018-04-07 19:27:49 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+function func() {
+    window.alert("欢迎你来到这里");
+    window.alert("那么");
+    window.alert("我要恭喜你");
+    window.alert("你被整了！");
+    window.alert("这个网站可能是你的朋友发来的");
+    window.alert("但是");
+    window.alert("请不要逃避现实");
+    var j = 10000;
+    for(var i=0;i<10000;i--){
+        window.alert("享受吧！！！" + "还有" + j + "次");
+        j--;
+    }
+}
